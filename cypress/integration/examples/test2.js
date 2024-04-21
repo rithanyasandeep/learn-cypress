e30bff8b60ad6136d2fd14ab7e87b1ff7aa8f633
rithanyasandeep <rithanyasenthilkumar19@gmail.com>
2024-04-21 19:34:47 +0530

    spec files

---
+++ cypress/integration/examples/test2.js
@@ -0,0 +1,27 @@
+describe('test suit',function()
+{
+    it('2nd test case', function()
+{
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
+    cy.wait(1000)
+    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+    //dynamically select the name of the product by .each() method [LOOP Iterator]
+    cy.get('.products').find('.product').each(($el, index, $list)=>
+    {
+        const prodName = $el.find('.product-name').text()
+        if (prodName.includes('Cashews'))
+        {
+            //to compromise the promise wrap() is used
+            cy.wrap($el).find('button').click()
+            /* another way 
+            cy.wrap($el).contains('ADD TO CART').click()
+            */
+        }
+    })
+    cy.get('.cart-icon').click()
+    cy.contains('PROCEED TO CHECKOUT').click()
+    cy.contains('Place Order').click()
+
+})
+})
