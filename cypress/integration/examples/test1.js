6d49399dd080922f81d5a693ba34be50e7d7afff
rithanyasandeep <rithanyasenthilkumar19@gmail.com>
2024-04-22 22:09:10 +0530

    spec changes

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,62 +1,58 @@
 /// <reference types="Cypress" />
 
-describe("test suit", function() {
+describe("test suit", function () {
 
-    it("1st test case", function()
-{
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-    cy.url().should('includes','seleniumPractise')
-    /*     another way
-    =>the correct assertion command to check for partial inclusion in Cypress is include, not contains
+    it("1st test case", function () {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.url().should('includes', 'seleniumPractise')
+        /*     another way
+        =>the correct assertion command to check for partial inclusion in Cypress is include, not contains
+    
+        cy.url().should('contains','seleniumPractise')
+        // to verify the entire url is exactly correct
+        cy.url().should('eq', 'https://rahulshettyacademy.com/seleniumPractise/#/')
+        */
+        cy.get('.search-keyword').type('ca')
+        cy.wait(1000)
+        //take invisible element
+        cy.get('.product').should('have.length', 5)
+        //to pick only the visible element using JQ method
+        cy.get('.product:visible').should('have.length', 4)
+        //using alias method to replce repetition
+        cy.get('.products').as('productMainLocator')
+        //PARENT CHILD Chaining-restrain the search to specific block by .find()
+        cy.get('@productMainLocator').find('.product').should('have.length', 4)
+        //select products by the index EQ() method is used 
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+        //dynamically select the name of the product by .each() method [LOOP Iterator]
+        cy.get('@productMainLocator').find('.product').each(($el, index, $list) => {
+            const prodName = $el.find('.product-name').text()
+            if (prodName.includes('Cashews')) {
+                //to compromise the promise wrap() is used
+                cy.wrap($el).find('button').click()
+                /* another way 
+                cy.wrap($el).contains('ADD TO CART').click()
+                */
+            }
 
-    cy.url().should('contains','seleniumPractise')
-    // to verify the entire url is exactly correct
-    cy.url().should('eq', 'https://rahulshettyacademy.com/seleniumPractise/#/')
-    */
-    cy.get('.search-keyword').type('ca')
-    cy.wait(1000)
-    //take invisible element 
-    cy.get('.product').should('have.length',5)
-    //to pick only the visible element using JQ method
-    cy.get('.product:visible').should('have.length',4)
-    //using alias method to replce repetition
-    cy.get('.products').as('productMainLocator')
-    //PARENT CHILD Chaining-restrain the search to specific block by .find()
-    cy.get('@productMainLocator').find('.product').should('have.length',4)
-    //select products by the index EQ() method is used 
-    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
-    //dynamically select the name of the product by .each() method [LOOP Iterator]
-    cy.get('@productMainLocator').find('.product').each(($el, index, $list)=>
+        })
+        /*cy.get('.brand').then(function(logoelement)
     {
-        const prodName = $el.find('.product-name').text()
-        if (prodName.includes('Cashews'))
-        {
-            //to compromise the promise wrap() is used
-            cy.wrap($el).find('button').click()
-            /* another way 
-            cy.wrap($el).contains('ADD TO CART').click()
-            */
+        cy.log(logoelement.text())
+     
+    })*/
+        //TYPE-2
+        const nameLogo = (a) => {
+            cy.log(a.text())
         }
+        cy.get('.brand').then(nameLogo)
+        //assert the text
+        cy.get('.brand').should('have.text', 'GREENKART')
 
+        /* TYPE-3
+        cy.get('.brand').then((a)=>
+        {
+            cy.log(a.text())
+        })*/
     })
-    /*cy.get('.brand').then(function(logoelement)
-{
-    cy.log(logoelement.text())
- 
-})*/
-//TYPE-2
-const nameLogo= (a)=>
-{
-    cy.log(a.text())
-}
-cy.get('.brand').then(nameLogo)
-//assert the text
-cy.get('.brand').should('have.text','GREENKART')
-
-/* TYPE-3
-cy.get('.brand').then((a)=>
-{
-    cy.log(a.text())
-})*/
-})
 })
---
+++ cypress/integration/examples/test3.js
@@ -0,0 +1,40 @@
+describe("test suit", function () {
+
+    it("3rd test case", function () {
+
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.url().should('includes', 'AutomationPractice')
+
+        //checkboxes 
+        //to choose checkboxes
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        //to unselect the checkboxes
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value', 'option1')
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+        //radiobutton
+        cy.get('[value="radio2"]').check().should('be.checked')
+
+        //static dropdown
+        cy.get('select').select('option3').should('have.value', 'option3')
+
+        //dynamic dropdown
+        cy.get('#autocomplete').type('rit')
+        cy.get('li .ui-menu-item-wrapper').each(($el, index, $list) => {
+            if ($el.text() == 'Mauritania') {
+                cy.wrap($el).click()
+            }
+        })
+
+        //handle visible and invisible elements
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('not.be.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+
+
+
+
+    })
+})
